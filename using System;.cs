ac8bd74fc40169ee37b6c53d2cf2f656db72ab39
FireDragon8K <112441856+FireDragon8K@users.noreply.github.com>
2022-08-31 21:17:34 -0600

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string firstname ="Kai"; 
+      Console.Writeline(name); 
+
+      string lastName = "Ridge"; 
+      Console.WriteLine(name);
+     
+      int x = 11
+      int y = 10
+      int total = x + y;
+      Console.WriteLine(total);
+
+      int x = 7
+      int y = 9
+      int total = x + y;
+      Console.WriteLine(total):
+      
+      float right = 9, left = 6, straight = 2
+      float total = right + left + straight;
+      Console.WriteLine(total); 
+
+      float right = 4, left = 2, straight = 1
+      float total = right + left + straight;
+      Console.WriteLine(total);
+      
+      double myDoubleNum = 3D;
+      Console.WriteLine(double);
+
+      double myDoubleNum = 3D
+      Console.WriteLine(double);
+
+      char myLetter = 'L'
+      Console.WriteLine(Letter);
+
+      char myLetter ='O'
+      Console.WriterLine(Letter);
+
+      bool myBool = yes
+      Console.WriteLine(yes);
+
+      bool myBool = no
+      Console.WriteLine(yes)
+
+
+
+    } 
+
+  }
+}
