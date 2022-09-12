3ed8b11045ff7724111150090d9f6c3478060f50
FireDragon8K <112441856+FireDragon8K@users.noreply.github.com>
2022-09-11 18:37:12 -0600

    Add files via upload

---
+++ Balloon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+
+    public int clickToPop = 3; // how many clicks until the balloon pops
+
+    public float scaleToIncrease = 0.10f; // each time the balloon is clicked inflate 10% 
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        
+    }
+
+     void OnMouseDown()
+    {
+        clickToPop -= 1; // Reduce clicks by one
+
+        //Inflate the balloon a certain amount every time it is clicked on
+        Transform.localScale += vector3.one * scaleToIncrease;
+
+        if (clickToPop == 0)
+        {
+            Destroy(gameObject);
+        }
+    
+    }
+}
+
